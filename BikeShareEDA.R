ca9ad2418aa924beb9f1324a790099af7b8aa0ab
Peter Campbell <peterhcampbell06@gmail.com>
2023-09-08 14:01:55 -0600

    added initial files

---
+++ BikeShareEDA.R
@@ -0,0 +1,11 @@
+##
+## Bike Share EDA Code
+##
+
+## libraries
+library(tidyverse)
+library(vroom)
+
+## Read in the Data
+bike <- read.csv("c://Users/peter/Desktop/BYU School/fall 2023/stat348/bike-sharing-demand/train.csv")
+
