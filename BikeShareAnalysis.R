fa9f40414416fc00f90f80b5b1b5eae0c9e566b8
Peter Campbell <peterhcampbell06@gmail.com>
2023-09-15 15:28:24 -0600

    added model and saved to csv for submission

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -14,21 +14,17 @@ my_recipe <- recipe(count ~ ., data = bike_train) %>%
   
   ## Feature Engineering Section
   ## make weather a factor
-  step_num2factor(weather, levels = c("Clear", "Few clouds", "Partly Cloudy", "Partly cloudy")) %>%
+  step_mutate(weather=factor(weather)) %>%
   ## create hour and minutes variable
   step_time(datetime, features = c("hour", "minute")) %>%
   ## get days of the week
   step_date(datetime, features = "dow") %>%
   ## make season a factor
-  step_num2factor(season, levels = c("Spring", "Summer", "Fall", "Winter")) %>%
+  step_mutate(season=factor(season)) %>%
   ## remove zero variance predictors
-  step_zv(all_predictors()) %>%
+  step_zv(all_predictors())
+  ## change weather of 4 into 3
   
-  ## Cleaning Section
-  ## get rid the one day with weather = 4
-  step_filter(weather != 4) %>%
-  ## get rid of registered and casual columns, unusable in prediction
-  prep()
 
 
 prep_train <- my_recipe %>%#set up processing using bike
@@ -36,7 +32,32 @@ prep_train <- my_recipe %>%#set up processing using bike
 
 bake(my_recipe, new_data = bike_test)
 
+dplyr::glimpse(prep_train)
+
 view(prep_train)
-?factor
 
+## make night day variable
+## make 4 weather into a three
+## find more changes to do
 
+my_mod <- linear_reg() %>% 
+  set_engine("lm")
+
+bike_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(my_mod) %>%
+  fit(data = bike_train)
+
+bike_predict <- predict(bike_workflow, new_data=bike_test)
+view(bike_predict)
+
+df %>%
+  [,2] <- as.numeric([,2])
+  pmax([,2], 0)
+
+view(df)
+
+?vroom_write
+df <- data.frame(id = bike_test$datetime, bike_predict$.pred)
+view(df)
+vroom_write(x = df, file = "submission.csv")
